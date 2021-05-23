35b9ba6e05d5d0fe859a0e1074ce71fd86ac047d
samainae <sasaclariss@gmail.com>
2021-05-23 12:38:17 +0200

    commencement du catalogue, avec 3 panels (gestion, recherche et affichage)

--- src/view/Catalogue.java
+++ src/view/Catalogue.java
@@ -3,8 +3,18 @@ package view;
 import javax.swing.*;
 
 public class Catalogue extends JPanel {
-    
+
+    JTable tableLivre = new JTable();
+
+    JButton ajout = new JButton("+");
+    JButton suppression = new Button("-");
+
+    JPanel gestion = new JPanel;
+    JPanel recherche = new JPanel;
+    JPanel affichage = new JPanel;
+
     public Catalogue() {
-    
+        gestion.setLayout(new GridLayout);
+
     }
 }
